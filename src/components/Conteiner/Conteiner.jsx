7f5e03b4826eff94cdf2e52a4a2ab04d56191b23
jaycikey <jaycikey@gmail.com>
2024-08-16 21:00:40 -0400

    testPrettierVCS

--- src/components/Conteiner/Conteiner.jsx
+++ src/components/Conteiner/Conteiner.jsx
@@ -1,15 +1,15 @@
-import { Box, useMediaQuery, useTheme } from "@mui/material";
-import BgImg from "../../BgImg/BgImg";
-import { useSelector } from "react-redux";
+import { Box, useMediaQuery, useTheme } from '@mui/material';
+import BgImg from '../../BgImg/BgImg';
+import { useSelector } from 'react-redux';
 
 const Container = ({ children }) => {
-  const brandWidth = useSelector((state) => state.width.brandWidth);
+  const brandWidth = useSelector(state => state.width.brandWidth);
 
   const theme = useTheme();
   const isXs = useMediaQuery(theme.breakpoints.down('md'));
 
   const contentStyles = {
-    position: "relative",
+    position: 'relative',
     padding: `0 0 0 ${isXs ? 28 : brandWidth}px`,
   };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,18 @@
-import { Box } from "@mui/material";
-import Game from "./Game/Game";
-import Greeting from "./Greeting/Greeting";
-import Conteiner from "../Conteiner/Conteiner";
-import useBreakpoint from "../../hooks/useBreakpoint";
+import { Box } from '@mui/material';
+import Game from './Game/Game';
+import Greeting from './Greeting/Greeting';
+import Conteiner from '../Conteiner/Conteiner';
+import useBreakpoint from '../../hooks/useBreakpoint';
 
 const Home = () => {
- const isXs = useBreakpoint('md')
+  const isXs = useBreakpoint('md');
 
   const foregroundStyles = {
-    position: "relative",
+    position: 'relative',
     zIndex: 1,
-    display: "flex",
-    flexDirection: `${isXs ? "column" : "row"}`,
-    gap:"150px",
+    display: 'flex',
+    flexDirection: `${isXs ? 'column' : 'row'}`,
+    gap: '150px',
   };
 
   return (
