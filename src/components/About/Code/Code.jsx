e2fbd1b865635387fbc61b43b8b6088792fb56a7
jaycikey <jaycikey@gmail.com>
2024-08-23 12:42:45 -0400

    strAbout

--- src/components/About/Code/Code.jsx
+++ src/components/About/Code/Code.jsx
@@ -8,11 +8,44 @@ const Code = () => {
   const codeSnippetStyles = {
     backgroundColor: theme.palette.background.paper,
   };
+  const codeInfoStyles = {
+    display: 'flex',
+    gap: 2,
+  };
+  const detailStyles = {
+    display: 'flex',
+    gap: 1,
+  };
+  const avatarStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 1,
+  };
+
+  const InfoStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 1,
+  };
 
   return (
     <Box>
-      <Box>Code snippet showcase</Box>
+      <Typography variant="body1">Code snippet showcase List</Typography>
       <Typography variant="body1">{`// Code snippet showcase:`}</Typography>
+      <Box sx={codeInfoStyles}>
+        <Box sx={avatarStyles}>
+          <Typography variant="body1">avatar</Typography>
+        </Box>
+        <Box sx={InfoStyles}>
+          <Typography variant="body1">@username</Typography>
+          <Typography variant="body1">Created 5 months ago</Typography>
+        </Box>
+        <Box sx={detailStyles}>
+          <Typography variant="body1">details</Typography>
+          <Typography variant="body1">stars</Typography>
+        </Box>
+      </Box>
+
       <Box sx={codeSnippetStyles}>
         <pre>
           <code>{`
@@ -22,6 +55,7 @@ function initializeGame() {
           `}</code>
         </pre>
       </Box>
+      <Typography>{`My work here was 5 months ago. It was for the project called “...”. Some other text can be placed here.`}</Typography>
       <Scroll />
     </Box>
   );
